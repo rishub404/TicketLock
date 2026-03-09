a9c3255c585b3035b36b09d0bca966f17dd44aa2
Rishav Pramanik <rishavpramanik17@gmail.com>
2026-03-10 03:27:47 +0530

    feat: add initial frontend and backend scaffold along with docker-compose

---
+++ backend/src/main/java/com/ticketlock/api/TicketLockApiApplication.java
@@ -0,0 +1,13 @@
+package com.ticketlock.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketLockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketLockApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ticketlock/api/TicketLockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketlock.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketLockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
