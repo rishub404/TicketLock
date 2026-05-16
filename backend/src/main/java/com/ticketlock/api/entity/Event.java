f4d8c64e0aacd48c540f6fe212546af6548aa525
Rishav Pramanik <rishavpramanik17@gmail.com>
2026-05-16 23:47:54 +0530

    feat: add Event entity

---
+++ backend/src/main/java/com/ticketlock/api/entity/Event.java
@@ -0,0 +1,13 @@
+package com.ticketlock.api.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "events")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Event {
+}
